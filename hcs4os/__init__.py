fb4f05dfb638d34628fb4c0f08c283eb94a3e870
Adrian Montag <adrimontag@gmail.com>
2026-08-28 18:53:23 +0200

    updated pyproject

--- hcs4os/__init__.py
+++ hcs4os/__init__.py
@@ -2,5 +2,6 @@ from . import classification_system
 from . import dataloaders
 from . import _shared
 from . import _data
+from . import agents
 
-__all__ = ["classification_system", "dataloaders"]
+__all__ = ["classification_system", "dataloaders", "agents"]
