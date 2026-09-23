5a8b07b508b4bdb8b34e7974c71ebeab281b4f87
adimo20 <adrimontag@gmail.com>
2026-09-23 09:16:58 +0200

    Enhance BaseRAGAgent to include hierarchy option
    Added support for hierarchy inclusion in search results.

--- hcs4os/agents/RAG/BaseAgentRAG.py
+++ hcs4os/agents/RAG/BaseAgentRAG.py
@@ -2,7 +2,7 @@ import dspy
 from ...classification_system import get_classification_system
 from .vector_database import VectorStore
 import json
-
+dspy.settings.configure(adapter=dspy.ChatAdapter())
 class BaseRAGAgent(dspy.Module):
     
     def __init__(
@@ -17,7 +17,8 @@ class BaseRAGAgent(dspy.Module):
         codes_to_include:list[str]|None=None,
         api_base:str|None=None,
         chromadb_path:str="./data/chroma",
-        create_new_collection:bool=True,             
+        create_new_collection:bool=True, 
+        include_hierarchy=False            
     )->None:
 
         assert api_key is not None
@@ -28,13 +29,26 @@ class BaseRAGAgent(dspy.Module):
         self.signature = signature
         self.search_category_tool_doc = search_category_tool_doc
         self.codes_to_include = codes_to_include
+        self.include_hierarchy = include_hierarchy
 
         self.lm = dspy.LM(
             api_key=api_key,
             api_base=api_base,
-            model=model_name
+            model=model_name,
+            model_type="chat",
+            max_tokens=2048,   
+            temperature=0,  
+            timeout=120,       
+            num_retries=3
         )
-        dspy.configure(lm=self.lm)
+        dspy.configure(
+            lm=self.lm,
+            adapter=dspy.ChatAdapter(), 
+            provide_traceback=False,# Turn on for debugging when using quanitzed models, there are frequently appearing errors, due to the json scheme,
+            disable_history=False,
+            max_errors=10000,
+            max_iters=5
+        )
 
         self.classification_name = classification_name
         self.classification_system = get_classification_system(
@@ -60,6 +74,7 @@ class BaseRAGAgent(dspy.Module):
                     desc= self.search_category_tool_doc
                 )
             ],
+            max_iters=5
         )
         
     def index(
@@ -94,17 +109,36 @@ class BaseRAGAgent(dspy.Module):
     def search_category(
         self,
         query:str,
-        k:int
+        k:int,
+        where:dict|None=None
     ):
         result = self.store.collection.query(
             query_texts=[query],
-            n_results=k
+            n_results=k,
+            where=where
         )
+
         if result is not None:
-            output = result["metadatas"][0] # type: ignore
-            docs = result["documents"][0] # type: ignore
-            for o, d in zip(output, docs):
-                o["description"] = d # type: ignore
+            
+            metas = result["metadatas"][0] 
+            docs = result["documents"][0]
+
+            output = []
+
+            for d, m in zip(docs, metas):
+
+                curr_code = {
+                    "code":m["code"],
+                    "description":d
+                }
+                if self.include_hierarchy:
+                    
+                    includes_info = json.loads(m["details"]) if isinstance(m["details"], str) else m["details"]
+                    
+                    curr_code["includes"] = list(set(includes_info.get("includes",[])))
+
+                output.append(curr_code)
+
             return output
         else:
             raise Exception("Error occured while searching for code")
